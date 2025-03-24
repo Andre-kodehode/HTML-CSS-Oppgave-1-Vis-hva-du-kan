73388a12057e2e47977e76340a4c3f770ab43172
Andre-kodehode <AAA03022025@gmail.com>
2025-03-24 09:34:11 +0100

    Duplicate button on Forbidden page

--- Events.js
+++ Events.js
@@ -6,8 +6,21 @@ function revealText() {
   }
 }
 
-function revealMore() {
-  var readMore = document.getElementById("readMore");
+function forbiddenBtnReveal1() {
+  var readMore = document.getElementById("forbiddenReadMore1");
+  var btnText = document.getElementById("reveal-btn");
+  if (readMore.style.display === "inline") {
+    btnText.innerHTML = "Unravel rest of the page";
+    readMore.style.display = "none";
+  } else {
+    readMore.style.display = "none";
+    btnText.innerHTML = "Unravel less";
+    readMore.style.display = "inline";
+  }
+}
+
+function forbiddenBtnReveal2() {
+  var readMore = document.getElementById("forbiddenReadMore2");
   var btnText = document.getElementById("reveal-btn");
   if (readMore.style.display === "inline") {
     btnText.innerHTML = "Unravel rest of the page";
